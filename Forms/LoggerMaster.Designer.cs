e4e90bc5099ffb299993b1afd09ea73f9dfa6e58
ITz_VIKKY <49977662+vikky212001@users.noreply.github.com>
2023-10-15 00:56:29 +0530

    Logger Master Started

---
+++ Forms/LoggerMaster.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Med_Preserve.Forms
+{
+    partial class LoggerMaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.loggerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loggerTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.noOfSensorsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.assignToDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.intervalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.s1NameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.s2NameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.s3NameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.s4NameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isActiveDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.createdDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loggerMasterBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.med_PreserveDataSet = new Med_Preserve.Med_PreserveDataSet();
+            this.loggerMasterTableAdapter = new Med_Preserve.Med_PreserveDataSetTableAdapters.LoggerMasterTableAdapter();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loggerMasterBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.med_PreserveDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Logger ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.loggerIDDataGridViewTextBoxColumn,
+            this.loggerTypeDataGridViewTextBoxColumn,
+            this.noOfSensorsDataGridViewTextBoxColumn,
+            this.assignToDataGridViewTextBoxColumn,
+            this.intervalDataGridViewTextBoxColumn,
+            this.s1NameDataGridViewTextBoxColumn,
+            this.s2NameDataGridViewTextBoxColumn,
+            this.s3NameDataGridViewTextBoxColumn,
+            this.s4NameDataGridViewTextBoxColumn,
+            this.isActiveDataGridViewCheckBoxColumn,
+            this.createdDateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.loggerMasterBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(740, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // loggerIDDataGridViewTextBoxColumn
+            // 
+            this.loggerIDDataGridViewTextBoxColumn.DataPropertyName = "LoggerID";
+            this.loggerIDDataGridViewTextBoxColumn.HeaderText = "LoggerID";
+            this.loggerIDDataGridViewTextBoxColumn.Name = "loggerIDDataGridViewTextBoxColumn";
+            // 
+            // loggerTypeDataGridViewTextBoxColumn
+            // 
+            this.loggerTypeDataGridViewTextBoxColumn.DataPropertyName = "LoggerType";
+            this.loggerTypeDataGridViewTextBoxColumn.HeaderText = "LoggerType";
+            this.loggerTypeDataGridViewTextBoxColumn.Name = "loggerTypeDataGridViewTextBoxColumn";
+            // 
+            // noOfSensorsDataGridViewTextBoxColumn
+            // 
+            this.noOfSensorsDataGridViewTextBoxColumn.DataPropertyName = "NoOfSensors";
+            this.noOfSensorsDataGridViewTextBoxColumn.HeaderText = "NoOfSensors";
+            this.noOfSensorsDataGridViewTextBoxColumn.Name = "noOfSensorsDataGridViewTextBoxColumn";
+            // 
+            // assignToDataGridViewTextBoxColumn
+            // 
+            this.assignToDataGridViewTextBoxColumn.DataPropertyName = "AssignTo";
+            this.assignToDataGridViewTextBoxColumn.HeaderText = "AssignTo";
+            this.assignToDataGridViewTextBoxColumn.Name = "assignToDataGridViewTextBoxColumn";
+            // 
+            // intervalDataGridViewTextBoxColumn
+            // 
+            this.intervalDataGridViewTextBoxColumn.DataPropertyName = "Interval";
+            this.intervalDataGridViewTextBoxColumn.HeaderText = "Interval";
+            this.intervalDataGridViewTextBoxColumn.Name = "intervalDataGridViewTextBoxColumn";
+            // 
+            // s1NameDataGridViewTextBoxColumn
+            // 
+            this.s1NameDataGridViewTextBoxColumn.DataPropertyName = "S1Name";
+            this.s1NameDataGridViewTextBoxColumn.HeaderText = "S1Name";
+            this.s1NameDataGridViewTextBoxColumn.Name = "s1NameDataGridViewTextBoxColumn";
+            // 
+            // s2NameDataGridViewTextBoxColumn
+            // 
+            this.s2NameDataGridViewTextBoxColumn.DataPropertyName = "S2Name";
+            this.s2NameDataGridViewTextBoxColumn.HeaderText = "S2Name";
+            this.s2NameDataGridViewTextBoxColumn.Name = "s2NameDataGridViewTextBoxColumn";
+            // 
+            // s3NameDataGridViewTextBoxColumn
+            // 
+            this.s3NameDataGridViewTextBoxColumn.DataPropertyName = "S3Name";
+            this.s3NameDataGridViewTextBoxColumn.HeaderText = "S3Name";
+            this.s3NameDataGridViewTextBoxColumn.Name = "s3NameDataGridViewTextBoxColumn";
+            // 
+            // s4NameDataGridViewTextBoxColumn
+            // 
+            this.s4NameDataGridViewTextBoxColumn.DataPropertyName = "S4Name";
+            this.s4NameDataGridViewTextBoxColumn.HeaderText = "S4Name";
+            this.s4NameDataGridViewTextBoxColumn.Name = "s4NameDataGridViewTextBoxColumn";
+            // 
+            // isActiveDataGridViewCheckBoxColumn
+            // 
+            this.isActiveDataGridViewCheckBoxColumn.DataPropertyName = "IsActive";
+            this.isActiveDataGridViewCheckBoxColumn.HeaderText = "IsActive";
+            this.isActiveDataGridViewCheckBoxColumn.Name = "isActiveDataGridViewCheckBoxColumn";
+            // 
+            // createdDateDataGridViewTextBoxColumn
+            // 
+            this.createdDateDataGridViewTextBoxColumn.DataPropertyName = "CreatedDate";
+            this.createdDateDataGridViewTextBoxColumn.HeaderText = "CreatedDate";
+            this.createdDateDataGridViewTextBoxColumn.Name = "createdDateDataGridViewTextBoxColumn";
+            // 
+            // loggerMasterBindingSource
+            // 
+            this.loggerMasterBindingSource.DataMember = "LoggerMaster";
+            this.loggerMasterBindingSource.DataSource = this.med_PreserveDataSet;
+            // 
+            // med_PreserveDataSet
+            // 
+            this.med_PreserveDataSet.DataSetName = "Med_PreserveDataSet";
+            this.med_PreserveDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // loggerMasterTableAdapter
+            // 
+            this.loggerMasterTableAdapter.ClearBeforeFill = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Logger Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "No of Sensors";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Assign To";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Interval";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Sensor 1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 322);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Sensor 2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 348);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Sensor 3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 370);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Sensor 4";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(93, 272);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 297);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // LoggerMaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "LoggerMaster";
+            this.Text = "Logger Master";
+            this.Load += new System.EventHandler(this.LoggerMaster_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loggerMasterBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.med_PreserveDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Med_PreserveDataSet med_PreserveDataSet;
+        private System.Windows.Forms.BindingSource loggerMasterBindingSource;
+        private Med_PreserveDataSetTableAdapters.LoggerMasterTableAdapter loggerMasterTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loggerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loggerTypeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn noOfSensorsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn assignToDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn intervalDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn s1NameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn s2NameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn s3NameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn s4NameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isActiveDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn createdDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
